fc27f56611d5ad2542e3ddcfccb64577d9a28aab
Juanma469 <jmbasualdo05@gmail.com>
2022-07-28 19:53:11 -0300

    creacion componentes principales

--- front/porfolio-web/src/app/app.module.ts
+++ front/porfolio-web/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
+import { ExperienciaComponent } from './component/experiencia/experiencia.component';
+import { HardsoftskillsComponent } from './component/hardsoftskills/hardsoftskills.component';
+import { ProyectosComponent } from './component/proyectos/proyectos.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { AcercadeComponent } from './component/acercade/acercade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    HardsoftskillsComponent,
+    ProyectosComponent,
+    FooterComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
